373bd0322aa5f552f3b9c5348ea024a62f05777d
Arslon <arslonbekefsun09@gmail.com>
2025-06-20 23:42:03 +0300

    OSINT-Platform

--- osint_platform/core/services/whois_check.py
+++ osint_platform/core/services/whois_check.py
@@ -1,12 +1,19 @@
 import whois
 
+def serialize_date(date):
+    if isinstance(date, list):
+        return [d.strftime("%Y-%m-%d") for d in date]
+    elif hasattr(date, "strftime"):
+        return date.strftime("%Y-%m-%d")
+    return str(date)
+
 def check_domain(domain):
     try:
         data = whois.whois(domain)
         return {
             "Registrar": data.registrar,
-            "CreationDate": str(data.creation_date),
-            "ExpirationDate": str(data.expiration_date),
+            "CreationDate": serialize_date(data.creation_date),
+            "ExpirationDate": serialize_date(data.expiration_date),
             "NameServers": data.name_servers if isinstance(data.name_servers, list) else [data.name_servers],
         }
     except Exception as e:
